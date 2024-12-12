60e71513fe3f90b2e8d71efd978757b1a9a6ef78
Ram Roy <ramroycombd@gmail.com>
2024-12-13 00:33:16 +0600

    navbar complited

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,66 +1,83 @@
 import Image from "next/image";
 import Link from "next/link";
+import { CiSearch, CiShoppingCart } from "react-icons/ci";
 
 const Navbar = () => {
+  "";
+
+  const navItems = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Services",
+      path: "/services",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+  ];
   return (
-    <div className="navbar bg-slate-200">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+    <div className=" bg-slate-200 ">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Item 2</a>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <Link href="/">
+            <Image
+              alt="logo"
+              src="/assets/logo.png"
+              height={60}
+              width={100}
+            ></Image>
+          </Link>
         </div>
-        <Link href="/">
-          <Image
-            alt="logo"
-            src="/assets/logo.png"
-            height={60}
-            width={100}
-          ></Image>
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+        <div className="flex space-x-6">
+          {navItems.map((item) => (
+            <Link
+              className="font-semibold hover:text-primary hover:animate-pulse duration-300"
+              href={item.path}
+              key={item.path}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </div>
+        <div className="navbar-end">
+          <dir className="flex items-center space-x-3 ">
+            <CiSearch className="text-2xl hover:text-orange-300 hover:animate-pulse" />
+            <CiShoppingCart className="text-2xl hover:text-orange-300 hover:animate-pulse" />
+            <a className="btn btn-outline btn-primary hover:animate-pulse px-4">
+              Appointment
+            </a>
+          </dir>
+        </div>
       </div>
     </div>
   );
